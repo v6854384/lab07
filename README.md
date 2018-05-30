e08df2fdaa96fbb3ffc2a153a07a1392eb0e73b8
v6854384 <v6854384@mail.ru>
2018-05-30 20:59:53 +0300

    added documentation

--- README.md
+++
@@ -1,2 +0,0 @@
-[![Build Status](https://travis-ci.org/v6854384/lab07.svg?branch=master)](https://travis-ci.org/v6854384/lab07)
-# lab07
--- _build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/v6854384/reports/lab06/projects/lab06
-# Build directory: /home/v6854384/reports/lab06/projects/lab06/_build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(check "/home/v6854384/reports/lab06/projects/lab06/_build/check" "-s" "-r" "compact" "--use-colour" "yes")
